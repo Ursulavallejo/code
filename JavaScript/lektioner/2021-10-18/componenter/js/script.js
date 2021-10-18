3a6ad49c479c45badc93efe3f49c466489215d7f
ursula_vallejo <ursulavallejo@gmail.com>
2021-10-18 21:08:14 +0200

    klass

--- JavaScript/lektioner/2021-10-18/componenter/js/script.js
+++ JavaScript/lektioner/2021-10-18/componenter/js/script.js
@@ -8,15 +8,42 @@ function consoleLogSomething(){
 // Event Listener
 document.getElementById('event-listener-1').addEventListener('click',function (){
     console.log('Event listener 1 clicked')
-})
+});
 
-function eventListenerTwo (text){
+function eventListenerTwo (){
     console.log('Event listener 2 clicked')
 }
 document.getElementById('event-listener-2').addEventListener("click", eventListenerTwo)
 
 // Hämta data från ett fält
-function nameFieldButton (){
+function nameFieldButton(){
     let nameField = document.getElementById('name-field').value
-    console.log('nameFieldButton clicked, content off input:',nameField)
+    // console.log('nameFieldButton clicked, "value" of input:', nameField)
+
+
+let helloNameField = document.getElementById('hello-name-field')
+//console.log(helloNameField)
+// document.getElementById('hello-name-field').innerText = 'Hej '
+// helloNameField.innerText = `Hej ${nameField}!`
+// helloNameField.innerText = `<strong>Hej ${nameField}!</strong>`
+helloNameField.innerHTML = `<strong>Hej ${nameField}!</strong>`
 }
+
+function add(x,y){
+    return x + y
+}
+
+function addNumbers(){
+    const number1 = Number(document.getElementById('num-field1').value)
+    const number2 = Number(document.getElementById('num-field2').value)
+    const sum = document.getElementById('sum-num-field')
+
+    sum.innerText = add(number1,number2)
+}
+
+const morE = 'more'
+
+function somethingMoreToLoadOnPageRender() {
+    document.getElementById('more').innerText = morE.toUpperCase()
+}
+somethingMoreToLoadOnPageRender()
