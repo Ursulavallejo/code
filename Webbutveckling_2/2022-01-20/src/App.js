6fc87b3489947d3381df0a758bdb85c680629628
ursula_vallejo <ursulavallejo@gmail.com>
2022-01-20 21:41:35 +0100

    robot search

--- Webbutveckling_2/2022-01-20/src/App.js
+++ Webbutveckling_2/2022-01-20/src/App.js
@@ -1,12 +1,30 @@
 import CardList from "./components/CardList";
+import robots from "./utils/data/robots";
+import Scroll from "./components/scroll/Scroll";
+import SearchBox from "./components/SearchBox";
+import {useState} from "react";
 
 function App() {
+    const [searchField, setSearchField] = useState('')
+
+    const filteredRobots = robots.filter(robot => {
+        return robot.name.toLowerCase().includes(searchField.toLowerCase())
+    })
     return (
         <main className='tc'>
             <h1 className='robo-logo f1'>Robofriends</h1>
-            <CardList/>
+            { searchField }
+            <SearchBox setSearchField={setSearchField} />
+            <Scroll>
+                <CardList robots={ filteredRobots }/>
+            </Scroll>
+
         </main>
     )
 }
 
 export default App;
+
+
+
+
