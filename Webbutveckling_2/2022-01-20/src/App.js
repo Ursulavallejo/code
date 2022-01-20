136018f80056476e478a718b65de7ef14d1a85f3
ursula_vallejo <ursulavallejo@gmail.com>
2022-01-20 12:22:41 +0100

    roboter

---
+++ Webbutveckling_2/2022-01-20/src/App.js
@@ -0,0 +1,12 @@
+import CardList from "./components/CardList";
+
+function App() {
+    return (
+        <main className='tc'>
+            <h1 className='robo-logo f1'>Robofriends</h1>
+            <CardList/>
+        </main>
+    )
+}
+
+export default App;
