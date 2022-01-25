bf62c72a7836878ec26013e9d20e977086a845c6
ursula_vallejo <ursulavallejo@gmail.com>
2022-01-25 11:59:28 +0100

    react from external Api

--- Webbutveckling_2/2022-01-25/frontend/src/views/FetchView.jsx
+++ Webbutveckling_2/2022-01-25/frontend/src/views/FetchView.jsx
@@ -1,8 +1,25 @@
+import {useEffect, useState} from "react";
+import  { JsonToTable} from "react-json-to-table";
+
 const FetchView = () => {
+    const [jsonUsers, setJsonUsers] = useState([])
+
+    function fetchUsers(){
+        fetch( 'https://jsonplaceholder.typicode.com/users ')
+            .then(response => response.json())
+            .then(jsonData => {
+                setJsonUsers(jsonData)
+            })
+    }
+
+    useEffect( () => {
+fetchUsers()
+    }, [])
     return (
 
         <>
             <h1>Fetch view</h1>
+            <JsonToTable json={jsonUsers}/>
         </>
     )
 }
