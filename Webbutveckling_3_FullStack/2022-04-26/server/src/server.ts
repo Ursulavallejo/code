77f471c41a8dd625fa3f0e1296914a4ef6a2fddb
Ursulavallejo <ursulavallejo@gmail.com>
2022-04-27 09:32:25 +0200

    backend TS

---
+++ Webbutveckling_3_FullStack/2022-04-26/server/src/server.ts
@@ -0,0 +1,26 @@
+import express from "express"
+import Configuration from "./configurations/Configuration"
+import Middleware from "./middlewares/Middleware"
+import Logger from "./utils/Logger";
+import AliveRoutes from "./routes/AliveRoutes";
+import UserRoutes from "./routes/UserRoutes";
+
+
+const  server = express()
+Middleware.applyMiddlewares(server)
+
+//Routes
+AliveRoutes(server)
+UserRoutes(server)
+
+Middleware.errorHandlerAndNotFound(server)
+
+Configuration.connectToPort(server)
+Configuration.connectToDatabase().then(()=>{
+    Logger.debug('--== lolz ==--')
+})
+
+
+export default server
+
+
